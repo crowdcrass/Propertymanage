857f3e0553df81268b61fd0346ee2aee1fb7cbcd
crowd <wy2424929101@163.com>
2015-05-19 16:34:39 +0800

    Initial Commit

---
+++ Propertymanage/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Propertymanage
+//
+//  Created by Kong xiaopeng on 15/5/19.
+//  Copyright (c) 2015年 junlian.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
---
+++ PropertymanageTests/PropertymanageTests.m
@@ -0,0 +1,40 @@
+//
+//  PropertymanageTests.m
+//  PropertymanageTests
+//
+//  Created by Kong xiaopeng on 15/5/19.
+//  Copyright (c) 2015年 junlian.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PropertymanageTests : XCTestCase
+
+@end
+
+@implementation PropertymanageTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
